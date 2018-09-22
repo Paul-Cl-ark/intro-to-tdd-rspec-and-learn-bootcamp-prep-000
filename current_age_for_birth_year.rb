1174905f356b2a4af507cf8d7f6f3b3a01258b43
Paul Clark <clark.paul123@gmail.com>
2018-09-22 08:46:18 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,5 +1,6 @@
+puts "What year were you born in?"
+birth_year = gets.chomp
 
-
-birthyear
-
-def current_age_for_birth_year()
+def current_age_for_birth_year(birth_year)
+  age = birth_year - 2018
+end
